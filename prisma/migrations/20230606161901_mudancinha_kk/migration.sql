55072343709f295e24ac2353bd72059f9593ff05
Caio Henrique <109510970+CaioHVectorA@users.noreply.github.com>
2023-06-06 17:41:08 +0000

    Update 11 files

---
+++ prisma/migrations/20230606161901_mudancinha_kk/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `HistId` on the `Like` table. All the data in the column will be lost.
+  - Added the required column `histId` to the `Like` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Like" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "histId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Like_histId_fkey" FOREIGN KEY ("histId") REFERENCES "Hist" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Like" ("id", "userId") SELECT "id", "userId" FROM "Like";
+DROP TABLE "Like";
+ALTER TABLE "new_Like" RENAME TO "Like";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
