936096e860aafc9472d7d9f3c0c151c9d88f64f4
Caio Henrique Oliveira Batista <caihebatista@gmail.com>
2023-06-06 21:29:49 -0300

    relacional entre comentario e usuario

--- src/modules/comment/useCases/createComment/createCommentController.ts
+++ src/modules/comment/useCases/createComment/createCommentController.ts
@@ -5,9 +5,9 @@ import { AppError } from "../../../../errors/AppError";
 export class CreateCommentController {
     async handle(req: Request, res: Response) {
         console.table(req.body)
-        const { content,comment_by,comment_in,userId }: createCommentDTO = req.body
+        const { content,comment_by,comment_in,username_author }: createCommentDTO = req.body
         const useCase = new CreateCommentUseCase()
-        const result = await useCase.execute({content,comment_by,comment_in,userId})
+        const result = await useCase.execute({content,comment_by,comment_in,username_author})
         if (result !== 100) throw new AppError('Algo deu errado.')
         return res.status(200).json({message: 'Comentário criado!'})
     }
--- src/modules/comment/useCases/createComment/createCommentUseCase.ts
+++ src/modules/comment/useCases/createComment/createCommentUseCase.ts
@@ -3,8 +3,9 @@ import { prisma } from "../../../../prisma.client";
 import { createCommentDTO } from "../../dtos/comment";
 
 export class CreateCommentUseCase {
-    async execute({ content,comment_by,comment_in,userId }: createCommentDTO): Promise<number> {
-        const comment = await prisma.comment.create({data: {content,comment_by,comment_in}})
+    async execute({ content,comment_by,comment_in,username_author }: createCommentDTO): Promise<number> {
+        //@ts-ignore
+        const comment = await prisma.comment.create({data: {content,comment_by,comment_in,username_author}})
         if (!comment) {
             throw new AppError('Ocorreu um erro.')
         }
