a290b0cfac2173551f0924b791d8743584c7043f
Caio Henrique <109510970+CaioHVectorA@users.noreply.github.com>
2023-06-17 00:38:54 +0000

    Update 3 files

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import "express-async-errors"
 import express, { NextFunction, Request, Response } from 'express'
 import { routes } from './routes'
 import { AppError } from "./errors/AppError"
-
+import cors from 'cors'
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
